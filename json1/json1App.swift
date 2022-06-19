6bc701f93b8b05f2592d9dd9e7b2c7f135db586a
kashiash <kashiash@gmail.com>
2022-06-19 19:01:47 +0200

    Initial Commit

---
+++ json1/json1App.swift
@@ -0,0 +1,17 @@
+//
+//  json1App.swift
+//  json1
+//
+//  Created by Jacek Placek on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct json1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
